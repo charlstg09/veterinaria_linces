1725153598ecb9771d93652ad95ae62cfa13b3b6
carlos felix <carlosflx901@gmail.com>
2022-11-26 22:42:50 -0700

    primer commit para guardar el proyecto

---
+++ Form1.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace veterinaria_linces_beta_1
+{
+    partial class Veterinaria_lince
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.TITULO = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.GrayText;
+            this.button1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button1.Location = new System.Drawing.Point(288, 248);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(94, 29);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "ENTER";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // TITULO
+            // 
+            this.TITULO.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.TITULO.Font = new System.Drawing.Font("Segoe UI Historic", 10.8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point);
+            this.TITULO.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.TITULO.Location = new System.Drawing.Point(245, 27);
+            this.TITULO.Name = "TITULO";
+            this.TITULO.Size = new System.Drawing.Size(202, 25);
+            this.TITULO.TabIndex = 1;
+            this.TITULO.Text = "VETERINARIA LINCE";
+            this.TITULO.Click += new System.EventHandler(this.TITULO_Click);
+            // 
+            // Veterinaria_lince
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.ClientSize = new System.Drawing.Size(719, 453);
+            this.Controls.Add(this.TITULO);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Veterinaria_lince";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Veterinaria Lince";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Button button1;
+        private Label TITULO;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,25 @@
+namespace veterinaria_linces_beta_1
+{
+    public partial class Veterinaria_lince : Form
+    {
+        public Veterinaria_lince()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TITULO_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
